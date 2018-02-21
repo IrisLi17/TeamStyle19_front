2b1f7b919f67c050055b174f7ae792a6e28e862d
Iris Li <461729541@qq.com>
2018-02-21 18:24:21 +0800

    modify validator

--- src/components/reg.vue
+++ src/components/reg.vue
@@ -23,6 +23,13 @@ import authSrv from '@/api/auth.js'
 export default {
   name: 'Reg',
   data(){
+    var mailcheck = (rule, value, callback) => {
+      if (value.match(/@mails.tsinghua.edu.cn$/)) {
+        callback();
+      } else {
+        callback(new Error("请输入清华邮箱"));
+      }
+    }
     return{
       form: {
         email:'',
@@ -31,14 +38,15 @@ export default {
       },
       rules:{
         email: [
-          {required: true, message: '请输入邮箱', trigger:'blur'},
-          { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
+          //{required: true, message: '请输入邮箱', trigger:'blur'},
+          //{ type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
+          {validator: mailcheck, trigger: 'blur,change'}
         ],
         username: [
           {required: true, message: '请输入用户名', trigger:'blur'}
         ],
         password: [
-          {required: true, message: '请输入密码', trigger:'blur'}
+          {required: true, min: 8, max: 20, message: '请输入8-20位密码', trigger:'blur,change'}
         ]
       }
     }
